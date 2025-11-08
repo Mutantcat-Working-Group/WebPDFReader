1ae89414ef92e6ad5eaa75700b925df096b2088f
孙达明 <83948628+tyza66@users.noreply.github.com>
2025-11-08 22:06:10 +0800

    Revert "feat:适配设备"
    This reverts commit 13308133c2374f431bbb7c5e7df1912f96bca254.

--- web-pdf-reader/src/main.tsx
+++ web-pdf-reader/src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './polyfills/url-parse'
 import App from './App.tsx'
-import 'core-js/stable/promise/with-resolvers';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
