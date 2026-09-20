cff3bcd37f3ea79476acec25ea1f8e7798c12284
tyza66 <shun_@outlook.com>
2026-09-20 19:30:58 +0800

    chore: bump version and add CI/release workflows

--- web-pdf-reader/eslint.config.js
+++ web-pdf-reader/eslint.config.js
@@ -19,5 +19,11 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_', caughtErrors: 'none' }],
+      'no-empty': ['error', { allowEmptyCatch: true }],
+    },
   },
 ])
